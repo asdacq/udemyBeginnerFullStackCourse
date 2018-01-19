c5a7dd3ce229719e23465cc322b8eff483fb02e5
asdacq <hhuang0815@gmail.com>
2018-01-19 01:41:13 -0500

    finished section 5

--- Section5Javascript/js/Exercise3.js
+++ Section5Javascript/js/Exercise3.js
@@ -1,16 +1,40 @@
 var selector = document.getElementById("options");
 //var output = selector.options[selector.selectedIndex].text;
 //console.log(output);
+var selectedOption;
+selector.addEventListener("click", function(){
+    selectedOption = selector.options[selector.selectedIndex].value;
+    console.log(selectedOption);
+});
 
 
-//selector.addEventListener("click", function(){
-//    console.log(selector.options[selector.selectedIndex].value);
-//});
+var form = document.getElementById("inputfield");
+var firstNumber = document.getElementById("firstNumber");
+var secondNumber = document.getElementById("secondNumber");
+var result = document.getElementById("result");
 
-var first = document.getElementById("first");
-var second = document.getElementById("second");
-var third = document.getElementById("third");
-var fourth = document.getElementById("fourth");
-var fifth = document.getElementById("fifth");
-
-var collaspe = document.getElementsByClassName("collaspe");
+form.addEventListener("submit", function(myEvent){
+    
+    var num1 = parseFloat(firstNumber.value);
+    var num2 = parseFloat(secondNumber.value);
+    if(!firstNumber.value || !secondNumber.value){
+        alert("Please input numbers");
+    }
+    else if(selectedOption == 1){
+        result.innerText = "Result: " + (num1 * .01) / num2 + "%";
+    }
+    else if(selectedOption == 2){
+        result.innerText = "Result: " + num1 / (num2 * .01) + "%";      
+    }
+    else if(selectedOption == 3){
+        result.innerText = "Result: " + num1 / num2 * 100 + "%";
+    }
+    else if(selectedOption == 4){
+        result.innerText = "Result: " + num2 / (num1 * .01) + "%";
+    }
+    else {
+        result.innerText =  "Result: " + num1 * .01 * num2 + "%";
+    }
+    
+    myEvent.preventDefault();
+});
